3e48d434d9382443cbb9d012f2669483a2f860ec
alexthegreat194 <alexander.gentry@aol.com>
2023-06-02 15:43:11 -0700

    first iteration

--- makesite.go
+++ makesite.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+type PageContent struct {
+	Heading string
+	Body string
+}
+
+type PageData struct {
+	TextFilePath string
+    TextFileName string
+    HTMLPagePath string
+    Content      PageContent
+}
+
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Starting...")
+	
+	filePath := "first-post.txt"
+
+	// read file
+	fileContents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(fileContents))
+
+	// parse data into template
+	t, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	// create new html file
+	newFile, err := os.Create("new.html")
+	if err != nil {
+		panic(err)
+	}
+
+	// add html to file
+	err = t.Execute(newFile, PageContent{
+		Heading: "Heading",
+		Body: string(fileContents),
+	})
+	if err != nil {
+		panic(err)
+	}
 }

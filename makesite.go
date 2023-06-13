7be9913a7bb7a462c59e2816ac4d4c0bb1dbbf4d
alexthegreat194 <alexander.gentry@aol.com>
2023-06-12 21:41:05 -0700

    prettier pages and nicer code

--- makesite.go
+++ makesite.go
@@ -12,7 +12,7 @@ import (
 
 type PageContent struct {
 	Heading string
-	Body string
+	Body []string
 }
 
 type PageData struct {
@@ -22,6 +22,72 @@ type PageData struct {
     Content      PageContent
 }
 
+func printArray(arr []string ) {
+	fmt.Print("[")
+	for _, v := range arr {
+		fmt.Printf("\"%v\", ", v)
+	}
+	fmt.Print("]\n")
+}
+
+func formatContent(text string) PageContent {
+	formattedLines := []string{}
+	buffer := ""
+	for _, char := range text {
+		if char == '\n' {
+			formattedLines = append(formattedLines, buffer)
+			// fmt.Println(formattedLines[0])
+			buffer = ""
+		} else {
+			buffer += string(char)
+			// fmt.Println(buffer)
+		}
+	}
+	if buffer != "" {
+		formattedLines = append(formattedLines, buffer)
+	}
+
+	// get rid of empty strings
+	finalList := []string{}
+	for _, v := range formattedLines {
+		if v != "" {
+			finalList = append(finalList, string(v))
+		}
+	}
+
+	data := PageContent{finalList[0], finalList[1:]}
+	fmt.Println(data)
+	return data
+}
+
+func readFile(path string) string {
+	// read file
+	fileContents, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return string(fileContents)
+}
+
+func generateSite(data PageContent, dest string, templateName string) {
+	// generate template from file
+	t, err := template.ParseFiles(templateName)
+	if err != nil {
+		panic(err)
+	}
+
+	// create new html file
+	newFile, err := os.Create(dest)
+	if err != nil {
+		panic(err)
+	}
+
+	// add html to file
+	err = t.Execute(newFile, data)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fmt.Println("Starting...")
@@ -32,37 +98,15 @@ func main() {
 		panic(errors.New("file needed"))
 	}
 
-	// read file
-	fileContents, err := ioutil.ReadFile(*filePath)
-	if err != nil {
-		panic(err)
-	}
 
-	// parse data into template
-	t, err := template.ParseFiles("template.tmpl")
-	if err != nil {
-		panic(err)
-	}
+	fileContents := readFile(*filePath)
+	content := formatContent(fileContents)
+	
 
 	// get file's name
 	periodIndex := strings.Index(*filePath, ".")
 	fileNameSlice := []rune(*filePath)
 	newFileName := string(fileNameSlice[:periodIndex]) + ".html"
 
-	fmt.Println(string(newFileName))
-
-	// create new html file
-	newFile, err := os.Create(newFileName)
-	if err != nil {
-		panic(err)
-	}
-
-	// add html to file
-	err = t.Execute(newFile, PageContent{
-		Heading: "Heading",
-		Body: string(fileContents),
-	})
-	if err != nil {
-		panic(err)
-	}
+	generateSite(content, newFileName, "template.tmpl")
 }
